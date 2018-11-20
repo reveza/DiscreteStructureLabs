49367a57bc424644dda1e3dfc491ee80bbe1c10c
Jonathan <jonathan.helias@polymtl.ca>
2018-11-20 14:23:01 -0500

    meolleur GUI

--- tp2/GUI.py
+++ tp2/GUI.py
@@ -3,12 +3,12 @@ from Automats import Automates
 
 class GUI(Frame):
     def __init__(self, master=None, graph = None):
-        Frame.__init__(self, master,width = 25, height = 20)
+        Frame.__init__(self, master, bg="red")
         self.sv = StringVar()
-        self.text = Text(self, width = 20, height = 15)
-        self.entry = Entry(self, textvariable = self.sv, width = 20)
+        self.text = Text(self, width = 15)
+        self.entry = Entry(self, textvariable = self.sv)
         self.entry.bind("<KeyRelease>", self.on_entry)
-        self.entry.pack()
+        self.entry.pack(fill=X)
         self.pack()
 
         self.automates = graph
--- tp2/main.py
+++ tp2/main.py
@@ -2,11 +2,13 @@ from Automats import Automates
 from GUI import GUI
 from tkinter import *
 
+#Note: avec le lexique 6, il faut un peu de temps pour que les mots s'affichent a la premiere lettre
 auto = Automates("lexique5.txt")
 auto.currentNode = auto.depart.next('a').next('m').next('i')
 print(auto.printFromCurrentNode('ami'))
 auto.currentNode = auto.depart
 
 root = Tk()
+root.geometry('200x200')
 gui = GUI(root, auto)
 gui.mainloop()

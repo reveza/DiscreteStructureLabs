d49f1136e96ed6ccf0791eed8cbb45839052448c
Jonathan <jonathan.helias@polymtl.ca>
2018-12-03 13:04:47 -0500

    Push du code final

--- tp2/main.py
+++ tp2/main.py
@@ -1,13 +1,9 @@
 from Automats import Automates
 from GUI import GUI
 from tkinter import *
+#Note: il faut s'assurer d'avoir la librairie (doit être installé sur linux)
 
-#Note: avec le lexique 6, il faut un peu de temps pour que les mots s'affichent a la premiere lettre
-auto = Automates("lexique5.txt")
-auto.currentState = auto.start.next('a').next('m').next('i')
-print(auto.printFromCurrentState('ami'))
-auto.currentState = auto.start
-
+auto = Automates("lexique6.txt")
 root = Tk()
 root.geometry('300x300')
 gui = GUI(root, auto)
